7506e78c0c952b417701495d79082a53d84c6b82
urbrioche <aafulin@gmail.com>
2022-08-03 19:14:17 +0800

    練習distinctUntilChanged

--- index.ts
+++ index.ts
@@ -4,7 +4,12 @@ import * as domUtils from './dom-utils';
 // 存取 API 資料的程式碼
 import * as dataUtils from './data-utils';
 import { fromEvent } from 'rxjs';
-import { debounceTime, map, switchMap } from 'rxjs/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap,
+} from 'rxjs/operators';
 
 const keyword$ = fromEvent(document.querySelector('#keyword'), 'input').pipe(
   map((event) => (event.target as HTMLInputElement).value)
@@ -20,7 +25,10 @@ const keyword$ = fromEvent(document.querySelector('#keyword'), 'input').pipe(
 // 使用switchMap解決nested subscribe
 keyword$
   .pipe(
+    // 避免一有事件就查詢
     debounceTime(700),
+    // 避免重複查詢
+    distinctUntilChanged(),
     switchMap((keyword) => dataUtils.getSuggestions(keyword))
   )
   .subscribe((suggestions) => {
