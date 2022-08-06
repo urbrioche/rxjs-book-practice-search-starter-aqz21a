acc5e1cf7f7a6b6b5ab93c16ed24faa38f49c2c6
urbrioche <aafulin@gmail.com>
2022-08-06 21:39:47 +0800

    sort and page

--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ import * as domUtils from './dom-utils';
 
 // 存取 API 資料的程式碼
 import * as dataUtils from './data-utils';
-import { BehaviorSubject, fromEvent, of } from 'rxjs';
+import { BehaviorSubject, combineLatest, fromEvent, merge, of } from 'rxjs';
 import {
   catchError,
   debounceTime,
   distinctUntilChanged,
   filter,
   map,
+  mapTo,
+  scan,
   shareReplay,
   startWith,
   switchMap,
@@ -69,6 +71,8 @@ const keywordForSearch$ = keyword$.pipe(take(1));
 //   switchMap((keyword) => dataUtils.getSearchResult(keyword))
 // );
 
+/* search by keyword 第一版
+
 // keyword沒輸入 按search會出錯(getSearchResult)，事件監聽也會停止
 // 即使重新key keyword，search button怎麼按都沒效
 // 用cathError解決 (書裡沒示範這個情境)
@@ -101,6 +105,14 @@ searchByKeyword$.subscribe((result) => {
   domUtils.fillSearchResult(result);
 });
 
+*/
+
+// search by keyword 第二版 (排序/分頁)
+const searchByKeyword$ = search$.pipe(
+  switchMap(() => keywordForSearch$),
+  filter((keyword) => !!keyword)
+);
+
 // 建立BehaviorSubject，預設使用stars進行降冪排序
 const sortBy$ = new BehaviorSubject({
   sort: 'stars',
@@ -127,4 +139,51 @@ fromEvent(document.querySelector('#sort-stars'), 'click').subscribe(() => {
 
 fromEvent(document.querySelector('#sort-forks'), 'click').subscribe(() => {
   changeSort('forks');
+});
+
+const perPage$ = fromEvent(document.querySelector('#per-page'), 'change').pipe(
+  map((event: Event) => {
+    const input = event.target as HTMLSelectElement;
+    return +input.value;
+  })
+);
+
+const previousPage$ = fromEvent(
+  document.querySelector('#previous-page'),
+  'click'
+).pipe(mapTo(-1));
+
+const nextPage$ = fromEvent(document.querySelector('#next-page'), 'click').pipe(
+  mapTo(1)
+);
+
+const page$ = merge(previousPage$, nextPage$).pipe(
+  scan((currentPageIndex, value) => {
+    const nextPage = currentPageIndex + value;
+    return nextPage < 1 ? 1 : nextPage;
+  }, 1)
+);
+
+const startSearch$ = combineLatest({
+  keyword: searchByKeyword$,
+  sort: sortBy$,
+  page: page$.pipe(startWith(1)),
+  perPage: perPage$.pipe(startWith(10)),
+});
+
+const searchResult$ = startSearch$.pipe(
+  switchMap(({ keyword, sort, page, perPage }) => {
+    return dataUtils.getSearchResult(
+      keyword,
+      sort.sort,
+      sort.order,
+      page,
+      perPage
+    );
+  })
+);
+
+searchResult$.subscribe((result) => {
+  console.log('fillSearchResult');
+  domUtils.fillSearchResult(result);
 });
